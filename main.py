dc58d98ecc69b1ca20e3b4f1c05bd7af729cfae0
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2021-07-18 21:31:09 +0630

    update

--- main.py
+++ main.py
@@ -29,5 +29,18 @@ csvFilePath = r'mmCOVID - Services.csv'
 jsonFilePath = r'mmCovid.json'
 csv_to_json(csvFilePath, jsonFilePath)
 
+def get_unique_service_types(csvFilePath, jsonFilePath):
+    jsonArray = []
+    with open(csvFilePath, encoding='utf8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        for row in csvReader:
+            row = row.get('Services')
+            jsonArray.append(row)
+    with open(jsonFilePath, 'w', encoding='utf8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4, ensure_ascii=False)
+        jsonf.write(jsonString)
 
+validationCsvFilePath = r'mmCOVID - Validation.csv'
+validationJsonFilePath = r'validation.json'
+get_unique_service_types(validationCsvFilePath, validationJsonFilePath)
     
