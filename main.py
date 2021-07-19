a271779c213c0aed6677b25f6c3498b0e3cf5777
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2021-07-19 21:02:30 +0630

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import csv 
 import json 
 
-def csv_to_json(csvFilePath, jsonFilePath):
+def csv_to_json(csvFilePath, jsonFilePath, serviceFilePath):
     jsonArray = []
-      
+    uniqueServices = []
     #read csv file
     with open(csvFilePath, encoding='utf8') as csvf: 
         #load csv file data using csv library's dictionary reader
@@ -12,6 +12,8 @@ def csv_to_json(csvFilePath, jsonFilePath):
         #convert each csv row into python dict
         for row in csvReader: 
             #add this python dict to json array
+            if row.get('service') and row.get('service') not in uniqueServices:
+                uniqueServices.append(row.get('service'))
             row = {'name':row.get('name_mm'),
                     'type':row.get('service'),
                     'address':row.get('address_mm'),
@@ -19,28 +21,33 @@ def csv_to_json(csvFilePath, jsonFilePath):
                     'phone':[row.get('phone1'), row.get('phone2'), row.get('phone3'),row.get('phone4'),row.get('phone5')]
             }
             jsonArray.append(row)
-  
+
     #convert python jsonArray to JSON String and write to file
     with open(jsonFilePath, 'w', encoding='utf8') as jsonf: 
         jsonString = json.dumps(jsonArray, indent=4, ensure_ascii=False)
         jsonf.write(jsonString)
+
+    with open(serviceFilePath, 'w', encoding='utf8') as jsons: 
+        validJsonString = json.dumps(uniqueServices, indent=4, ensure_ascii=False)
+        jsons.write(validJsonString)
           
 csvFilePath = r'mmCOVID - Services.csv'
 jsonFilePath = r'mmCovid.json'
-csv_to_json(csvFilePath, jsonFilePath)
-
-def get_unique_service_types(csvFilePath, jsonFilePath):
-    jsonArray = []
-    with open(csvFilePath, encoding='utf8') as csvf:
-        csvReader = csv.DictReader(csvf)
-        for row in csvReader:
-            row = row.get('Services')
-            jsonArray.append(row)
-    with open(jsonFilePath, 'w', encoding='utf8') as jsonf: 
-        jsonString = json.dumps(jsonArray, indent=4, ensure_ascii=False)
-        jsonf.write(jsonString)
-
-validationCsvFilePath = r'mmCOVID - Validation.csv'
 validationJsonFilePath = r'validation.json'
-get_unique_service_types(validationCsvFilePath, validationJsonFilePath)
-    
+csv_to_json(csvFilePath, jsonFilePath, validationJsonFilePath)
+
+# def get_unique_service_types(csvFilePath, jsonFilePath):
+#     jsonArray = []
+#     with open(csvFilePath, encoding='utf8') as csvf:
+#         csvReader = csv.DictReader(csvf)
+#         for row in csvReader:
+#             row = row.get('Services')
+#             jsonArray.append(row)
+#     with open(jsonFilePath, 'w', encoding='utf8') as jsonf: 
+#         jsonString = json.dumps(jsonArray, indent=4, ensure_ascii=False)
+#         jsonf.write(jsonString)
+
+# validationCsvFilePath = r'mmCOVID - Validation.csv'
+# validationJsonFilePath = r'validation.json'
+# get_unique_service_types(validationCsvFilePath, validationJsonFilePath)
+#     
